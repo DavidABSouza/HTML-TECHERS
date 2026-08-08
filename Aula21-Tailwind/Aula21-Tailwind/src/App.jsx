0d1a7471cdd63d307a300d9a22b2f5b791430962
DavidABSouza <superdave300py@gmail.com>
2026-08-08 16:11:53 +0100

    feat: Adição da Aula 21 sobre Tailwind

---
+++ Aula21-Tailwind/Aula21-Tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(){
+  return (
+    <div className='min-h-screen bg-fuchsia-200 flex items-center justify-center p-4'>
+      Perfil Gamer
+    </div>
+  )
+}
+
+export default App;
---
+++ Aula21-Tailwind/Aula21-Tailwind/src/components/PerfilGamer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Peak from './src/assets/PEAK.jpg'
+import DeRoGa from './src/assets/DRG.jpg'
+import HoKnSi from './src/assets/HoKnSi.jpg'
+import Detroit from './src/assets/Detroit.png'
+import Undertale from './src/assets/Undertale.png'
+
+import {UserPlus, MessageSquare, Trophy, Clock, Gamepad2} from 'lucide-react'
+
+const jogos = [
+    
+    {id: 1, titulo:'Peak', horas:'88h', capa: Peak},
+    {id: 2, titulo:'Deep Rock Galactic', horas:'21h', capa: DeRoGa},
+    {id: 3, titulo:'Hollow Knight: Silksong', horas:'270h', capa: HoKnSi},
+    {id: 4, titulo:'Detroit: Become Human', horas:'435h', capa: Detroit},
+    {id: 5, titulo:'Undertale', horas:'1225h', capa: Undertale}
+]
+
+export default function PerfilGamer(){
+
+}
