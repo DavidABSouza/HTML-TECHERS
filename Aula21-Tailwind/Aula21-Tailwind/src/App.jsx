fa0191173dfd781de717f57a5505375b40bf9bc2
DavidABSouza <superdave300py@gmail.com>
2026-08-22 16:00:19 +0100

    feat: Continuação da Aula 21 de Tailwind

--- Aula21-Tailwind/Aula21-Tailwind/src/App.jsx
+++ Aula21-Tailwind/Aula21-Tailwind/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import PerfilGamer from "./components/PerfilGamer";
 
 function App(){
   return (
     <div className='min-h-screen bg-fuchsia-200 flex items-center justify-center p-4'>
-      Perfil Gamer
+      <PerfilGamer/>
     </div>
   )
 }
--- Aula21-Tailwind/Aula21-Tailwind/src/components/PerfilGamer.jsx
+++ Aula21-Tailwind/Aula21-Tailwind/src/components/PerfilGamer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Peak from './src/assets/PEAK.jpg'
-import DeRoGa from './src/assets/DRG.jpg'
-import HoKnSi from './src/assets/HoKnSi.jpg'
-import Detroit from './src/assets/Detroit.png'
-import Undertale from './src/assets/Undertale.png'
+import Peak from '/src/assets/PEAK.jpg'
+import DeRoGa from '/src/assets/DRG.png'
+import HoKnSi from '/src/assets/HoKnSi.png'
+import Detroit from '/src/assets/Detroit.png'
+import Undertale from '/src/assets/Undertale.png'
 
 import {UserPlus, MessageSquare, Trophy, Clock, Gamepad2} from 'lucide-react'
 
@@ -17,5 +17,17 @@ const jogos = [
 ]
 
 export default function PerfilGamer(){
-
+    return (
+        <div className="w-full max-w-3xl bg-slate-900 border border-slate-800 rounded-2xl overflow-hidden shadow-2xl">
+            {/* 1. BANNER */}
+            <div className="h-40 w-full bg-slate-800">
+                <img src="https://pbs.twimg.com/media/DwJ2QXWWkAIqsCE.jpg" alt="Banner" className="w-full h-full object-cover opacity-60"/>
+            </div>
+            {/* 2. CONTEÚDO PRINCIPAL DO CARD */}
+            <div className="p-6 relative">
+                {/* CABEÇALHO (Avatar sobreposto e botões) */}
+                <div className="flex flex-col sm:flex-row sm:items-end justify-between gap-4 -mt-16 mb-6"></div>
+            </div>
+        </div>
+    )
 }
